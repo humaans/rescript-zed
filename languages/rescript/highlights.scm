b1daa515cff0fe8c0f59fbe612d0c02e3c858157
Karolis Narkevicius <karolis.n@gmail.com>
2024-04-15 22:49:43 +0100

    It's working! The syntax highlighting only, not yet the LSP

--- languages/rescript/highlights.scm
+++ languages/rescript/highlights.scm
@@ -1,72 +1,225 @@
-[
-    "if"
-    "then"
-    "else"
-    "let"
-    "in"
-    (case)
-    (of)
-    (backslash)
-    (as)
-    (port)
-    (exposing)
-    (alias)
-    (import)
-    (module)
-    (type)
-    (arrow)
- ] @keyword
+(comment) @comment
+
+; Identifiers
+;------------
+
+; Escaped identifiers like \"+."
+((value_identifier) @constant.macro
+ (#match? @constant.macro "^\\.*$"))
 
 [
-    (eq)
-    (operator_identifier)
-    (colon)
-] @operator
-
-(type_annotation(lower_case_identifier) @function)
-(port_annotation(lower_case_identifier) @function)
-(function_declaration_left(lower_case_identifier) @function.definition)
-
-(function_call_expr
-    target: (value_expr
-        name: (value_qid (lower_case_identifier) @function)))
-
-(exposed_value(lower_case_identifier) @function)
-(exposed_type(upper_case_identifier) @type)
-
-(field_access_expr(value_expr(value_qid)) @identifier)
-(lower_pattern) @variable
-(record_base_identifier) @identifier
+  (type_identifier)
+  (unit_type)
+  (list)
+  (list_pattern)
+] @type
 
 [
-    "("
-    ")"
-] @punctuation.bracket
+  (variant_identifier)
+  (polyvar_identifier)
+] @constant
+
+(record_type_field (property_identifier) @property)
+(record_field (property_identifier) @property)
+(object (field (property_identifier) @property))
+(object_type (field (property_identifier) @property))
+(member_expression (property_identifier) @property)
+(module_identifier) @namespace
+
+; Parameters
+;----------------
+
+(list_pattern (value_identifier) @parameter)
+(spread_pattern (value_identifier) @parameter)
+
+; String literals
+;----------------
 
 [
-    "|"
-    ","
+  (string)
+  (template_string)
+] @string
+
+(template_substitution
+  "${" @punctuation.bracket
+  "}" @punctuation.bracket) @embedded
+
+(character) @string.special
+(escape_sequence) @string.escape
+
+; Other literals
+;---------------
+
+[
+  (true)
+  (false)
+] @constant.builtin
+
+(number) @number
+(polyvar) @constant
+(polyvar_string) @constant
+
+; Functions
+;----------
+
+; parameter(s) in parens
+[
+ (parameter (value_identifier))
+ (labeled_parameter (value_identifier))
+] @parameter
+
+; single parameter with no parens
+(function parameter: (value_identifier) @parameter)
+
+; first-level descructuring (required for nvim-tree-sitter as it only matches direct
+; children and the above patterns do not match destructuring patterns in NeoVim)
+(parameter (tuple_pattern (tuple_item_pattern (value_identifier) @parameter)))
+(parameter (array_pattern (value_identifier) @parameter))
+(parameter (record_pattern (value_identifier) @parameter))
+
+; Meta
+;-----
+
+(decorator_identifier) @annotation
+
+(extension_identifier) @keyword
+("%") @keyword
+
+; Misc
+;-----
+
+(subscript_expression index: (string) @property)
+(polyvar_type_pattern "#" @constant)
+
+[
+  ("include")
+  ("open")
+] @include
+
+[
+  "as"
+  "export"
+  "external"
+  "let"
+  "module"
+  "mutable"
+  "private"
+  "rec"
+  "type"
+  "and"
+  "assert"
+  "await"
+  "with"
+  "lazy"
+  "constraint"
+] @keyword
+
+((function "async" @keyword))
+
+(module_unpack "unpack" @keyword)
+
+[
+  "if"
+  "else"
+  "switch"
+  "when"
+] @conditional
+
+[
+  "exception"
+  "try"
+  "catch"
+] @exception
+
+(call_expression
+  function: (value_identifier) @exception
+  (#eq? @exception "raise"))
+
+[
+  "for"
+  "in"
+  "to"
+  "downto"
+  "while"
+] @repeat
+
+[
+  "."
+  ","
+  "|"
 ] @punctuation.delimiter
 
-(number_constant_expr) @constant
+[
+  "++"
+  "+"
+  "+."
+  "-"
+  "-."
+  "*"
+  "**"
+  "*."
+  "/."
+  "<="
+  "=="
+  "==="
+  "!"
+  "!="
+  "!=="
+  ">="
+  "&&"
+  "||"
+  "="
+  ":="
+  "->"
+  "|>"
+  ":>"
+  "+="
+  (uncurry)
+] @operator
 
-(type_declaration(upper_case_identifier) @type)
-(type_ref) @type
-(type_alias_declaration name: (upper_case_identifier) @type)
-
-(value_expr(upper_case_qid(upper_case_identifier)) @type)
+; Explicitly enclose these operators with binary_expression
+; to avoid confusion with JSX tag delimiters
+(binary_expression ["<" ">" "/"] @operator)
 
 [
-    (line_comment)
-    (block_comment)
-] @comment
+  "("
+  ")"
+  "{"
+  "}"
+  "["
+  "]"
+] @punctuation.bracket
 
-(string_escape) @string.escape
+(polyvar_type
+  [
+   "["
+   "[>"
+   "[<"
+   "]"
+  ] @punctuation.bracket)
 
 [
-    (open_quote)
-    (close_quote)
-    (regular_string_part)
-    (open_char)
-    (close_char)
-] @string
+  "~"
+  "?"
+  "=>"
+  ".."
+  "..."
+] @punctuation.special
+
+(ternary_expression ["?" ":"] @operator)
+
+; JSX
+;----------
+(jsx_identifier) @tag
+(jsx_element
+  open_tag: (jsx_opening_element ["<" ">"] @tag.delimiter))
+(jsx_element
+  close_tag: (jsx_closing_element ["<" "/" ">"] @tag.delimiter))
+(jsx_self_closing_element ["/" ">" "<"] @tag.delimiter)
+(jsx_fragment [">" "<" "/"] @tag.delimiter)
+(jsx_attribute (property_identifier) @tag.attribute)
+
+; Error
+;----------
+
+(ERROR) @error
