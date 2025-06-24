4bcc734fd2cbaccd7127e745cb7854db53e6611e
Sam Rowe <sgrowe@live.co.uk>
2025-06-24 12:02:23 +0100

    Update queries to match zed naming conventions

--- languages/rescript/textobjects.scm
+++ languages/rescript/textobjects.scm
@@ -4,110 +4,110 @@
 ; Classes (modules)
 ;------------------
 
-(module_binding definition: ((_) @class.inner)) @class.outer
+(module_binding definition: ((_) @class.inside)) @class.around
 
 ; Blocks
 ;-------
 
-(block (_) @block.inner) @block.outer
+(block (_) @block.inside) @block.around
 
 ; Functions
 ;----------
 
-(function body: (_) @function.inner) @function.outer
+(function body: (_) @function.inside) @function.around
 
 ; Calls
 ;------
 
-(call_expression arguments: ((_) @call.inner)) @call.outer
+(call_expression arguments: ((_) @call.inside)) @call.around
 
 ; Comments
 ;---------
 
-(comment) @comment.outer
+(comment) @comment.around
 
 ; Parameters
 ;-----------
 
-(function parameter: (_) @parameter.inner @parameter.outer)
+(function parameter: (_) @parameter.inside @parameter.around)
 
 (formal_parameters
   "," @_formal_parameters_start
-  . (_) @parameter.inner
-  (#make-range! "parameter.outer" @_formal_parameters_start @parameter.inner))
+  . (_) @parameter.inside
+  (#make-range! "parameter.around" @_formal_parameters_start @parameter.inside))
 (formal_parameters
-  . (_) @parameter.inner
+  . (_) @parameter.inside
   . ","? @_formal_parameters_end
-  (#make-range! "parameter.outer" @parameter.inner @_formal_parameters_end))
+  (#make-range! "parameter.around" @parameter.inside @_formal_parameters_end))
 
 (arguments
   "," @_arguments_start
-  . (_) @parameter.inner
-  (#make-range! "parameter.outer" @_arguments_start @parameter.inner))
+  . (_) @parameter.inside
+  (#make-range! "parameter.around" @_arguments_start @parameter.inside))
 (arguments
-  . (_) @parameter.inner
+  . (_) @parameter.inside
   . ","? @_arguments_end
-  (#make-range! "parameter.outer" @parameter.inner @_arguments_end))
+  (#make-range! "parameter.around" @parameter.inside @_arguments_end))
 
 (function_type_parameters
   "," @_function_type_parameters_start
-  . (_) @parameter.inner
-  (#make-range! "parameter.outer" @_function_type_parameters_start @parameter.inner))
+  . (_) @parameter.inside
+  (#make-range! "parameter.around" @_function_type_parameters_start @parameter.inside))
 (function_type_parameters
-  . (_) @parameter.inner
+  . (_) @parameter.inside
   . ","? @_function_type_parameters_end
-  (#make-range! "parameter.outer" @parameter.inner @_function_type_parameters_end))
+  (#make-range! "parameter.around" @parameter.inside @_function_type_parameters_end))
 
 (functor_parameters
   "," @_functor_parameters_start
-  . (_) @parameter.inner
-  (#make-range! "parameter.outer" @_functor_parameters_start @parameter.inner))
+  . (_) @parameter.inside
+  (#make-range! "parameter.around" @_functor_parameters_start @parameter.inside))
 (functor_parameters
-  . (_) @parameter.inner
+  . (_) @parameter.inside
   . ","? @_functor_parameters_end
-  (#make-range! "parameter.outer" @parameter.inner @_functor_parameters_end))
+  (#make-range! "parameter.around" @parameter.inside @_functor_parameters_end))
 
 (type_parameters
   "," @_type_parameters_start
-  . (_) @parameter.inner
-  (#make-range! "parameter.outer" @_type_parameters_start @parameter.inner))
+  . (_) @parameter.inside
+  (#make-range! "parameter.around" @_type_parameters_start @parameter.inside))
 (type_parameters
-  . (_) @parameter.inner
+  . (_) @parameter.inside
   . ","? @_type_parameters_end
-  (#make-range! "parameter.outer" @parameter.inner @_type_parameters_end))
+  (#make-range! "parameter.around" @parameter.inside @_type_parameters_end))
 
 (type_arguments
   "," @_type_arguments_start
-  . (_) @parameter.inner
-  (#make-range! "parameter.outer" @_type_arguments_start @parameter.inner))
+  . (_) @parameter.inside
+  (#make-range! "parameter.around" @_type_arguments_start @parameter.inside))
 (type_arguments
-  . (_) @parameter.inner
+  . (_) @parameter.inside
   . ","? @_type_arguments_end
-  (#make-range! "parameter.outer" @parameter.inner @_type_arguments_end))
+  (#make-range! "parameter.around" @parameter.inside @_type_arguments_end))
 
 (decorator_arguments
   "," @_decorator_arguments_start
-  . (_) @parameter.inner
-  (#make-range! "parameter.outer" @_decorator_arguments_start @parameter.inner))
+  . (_) @parameter.inside
+  (#make-range! "parameter.around" @_decorator_arguments_start @parameter.inside))
 (decorator_arguments
-  . (_) @parameter.inner
+  . (_) @parameter.inside
   . ","? @_arguments_end
-  (#make-range! "parameter.outer" @parameter.inner @_arguments_end))
+  (#make-range! "parameter.around" @parameter.inside @_arguments_end))
 
 (variant_parameters
   "," @_variant_parameters_start
-  . (_) @parameter.inner
-  (#make-range! "parameter.outer" @_variant_parameters_start @parameter.inner))
+  . (_) @parameter.inside
+  (#make-range! "parameter.around" @_variant_parameters_start @parameter.inside))
 (variant_parameters
-  . (_) @parameter.inner
+  . (_) @parameter.inside
   . ","? @_variant_parameters_end
-  (#make-range! "parameter.outer" @parameter.inner @_variant_parameters_end))
+  (#make-range! "parameter.around" @parameter.inside @_variant_parameters_end))
 
 (polyvar_parameters
   "," @_polyvar_parameters_start
-  . (_) @parameter.inner
-  (#make-range! "parameter.outer" @_polyvar_parameters_start @parameter.inner))
+  . (_) @parameter.inside
+  (#make-range! "parameter.around" @_polyvar_parameters_start @parameter.inside))
 (polyvar_parameters
-  . (_) @parameter.inner
+  . (_) @parameter.inside
   . ","? @_polyvar_parameters_end
-  (#make-range! "parameter.outer" @parameter.inner @_polyvar_parameters_end))
+  (#make-range! "parameter.around" @parameter.inside @_polyvar_parameters_end))
